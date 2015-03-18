b9113fd7ff8454f00210f17f10bd0609d4734366
BrookeT <bthomas14@gmail.com>
2015-03-18 02:21:30 -0700

    First commit

--- app/app.js
+++ app/app.js
@@ -1,12 +1,39 @@
-'use strict';
+(function() {
+var app = angular.module('gemStore', ['store-products']);
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+app.controller('StoreController', [ '$http', function($http){
+  //this.products = $http({ method: 'GET', url: '/products.json' });
+  //$http.get('/products.json', { apiKey: 'myApiKey' });
+  var store = this;
+
+  store.products = [];
+  $http.get('store-products.json').success(function(data){
+    store.products = data;
+  });
+  //$http.post('/path/to/resource.json', { param: 'value' }); //post to server
+  //$http.delete('/path/to/resource.json'); //delete from server
+  //$http({ method: 'OPTIONS', url: '/path/to/resource.json' }); //post other methods to server using config object
+
 }]);
+
+/*
+app.controller('GalleryController', function(){
+
+  this.current = 0;
+
+  this.setCurrent = function(imageNumber){
+    this.current = imageNumber || 0;
+  };
+});
+*/
+app.controller("ReviewController", function(){
+  this.review = {};
+
+  this.addReview = function(product) {
+    this.review.createdOn = Date.now();
+    product.reviews.push(this.review);
+    this.review = {};
+  };
+});
+
+})();
